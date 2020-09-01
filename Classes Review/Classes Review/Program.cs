a9c3a598f1b35979e7a1b0f06545e4c03d26301e
April English <aenglish909@gmail.com>
2020-09-01 18:03:02 -0500

    Classes review

--- Classes Review/Classes Review/Program.cs
+++ Classes Review/Classes Review/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Transactions;
 
 namespace Classes_Review
 {
@@ -6,9 +8,34 @@ namespace Classes_Review
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
+            List<Student> students = new List<Student>();
+            Student stud1 = new Student();
+            //Console.WriteLine($"{stud1.name} has an id of {stud1.id}");
+            stud1.setName("Micah");  // the only wasy to et the id is to write a method in Students
 
+            stud1.id = 2;
+            students.Add(stud1);
+
+            Student stud2 = new Student("Adam", 1);
+            students.Add(stud2);
+
+            foreach (var stud in students)
+            {
+                Console.WriteLine($"{stud1.name} has an id of {stud1.id}");
+            }
         }
+
+        //Console.WriteLine($"{stud2.name} has an id of {stud2.id}");
+        //this shows the full info on the screen
+
+        DateTime current = DateTime.Now;
+        DateTime past = Convert.ToDateTime("1/1/2020");
+
+       
+           
+
+
+
     }
-}
+    }  
---
+++ Classes Review/Classes Review/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Review
+{
+    class Student //nameing the class
+    {
+        public string name { get; set; }  //declaring the variables 
+
+        public int id { get; set; } // take away the set and it makes it to where they cannot set the specific name.
+
+        public  static int totalStudents { get; set; }
+
+        public DateTime enrollDate { get; set; }
+
+        //creating methods
+
+        public Student(string name, int id)
+        {
+            this.name = name;                // green lines there is a variable called name
+            this.id = id;                // in the top class so you have to put this. in front of the variable 
+            enrollDate = DateTime.Now;  //names so that it knows to only refernce this name in this class
+
+        }
+
+        public Student()
+        {
+            name = string.Empty;
+            id = -1;    //by default all of our student are going to have an empty string for name until it is inouted 
+            enrollDate = DateTime.Now;  //have to call this in eahc method
+      
+        }
+
+        public string getName()  //get name from the user
+        {
+            return name;
+        }
+
+        public int getId()  // to return the id 
+        {
+            return id;
+        }
+
+        public void setName(string name)  // to set name 
+        {
+            this.name = name;
+        }
+
+        public override string ToString()  //to override
+        {
+            return ("programming is so much fun!");
+        }
+
+    }
+} // this is from the classes slides student from slide 17
